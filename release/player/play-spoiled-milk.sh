29fd15931f5b38070bef18caff665b99509fafb2
CardGame Developer <cardgame@example.com>
2026-06-05 21:57:17 -0400

    Fix player shell launcher updater invocation

--- release/player/play-spoiled-milk.sh
+++ release/player/play-spoiled-milk.sh
@@ -3,6 +3,6 @@ set -eu
 
 GAME_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
-"$GAME_DIR/update-spoiled-milk.sh"
+sh "$GAME_DIR/update-spoiled-milk.sh"
 
 exec java -jar "$GAME_DIR/Spoiled_Milk_Client.jar"
